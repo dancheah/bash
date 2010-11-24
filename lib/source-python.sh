387ce85837d6cbec533ab2a9eb577a835c806782
Daniel Cheah <me@dancheah.com>
2010-11-23 23:32:31 -0800

    added support for virtualenv

--- lib/source-python.sh
+++ lib/source-python.sh
@@ -2,4 +2,8 @@ function use_pythonbrew() {
     source ${HOME}/.pythonbrew/etc/bashrc
 }
 
+function use_virtualenv {
+    source ${HOME}/.pythonbrew/pythons/current/bin/virtualenvwrapper.sh
+}
+
 # vim: sts=4 sw=4 ts=4 et ft=sh
