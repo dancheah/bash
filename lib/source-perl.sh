e8bdea426f2d4cd5c1a098146e299a9d12c33613
Daniel Cheah <me@dancheah.com>
2011-07-30 14:46:42 -0700

    rejiggered my perlbrew and locallib paths

--- lib/source-perl.sh
+++ lib/source-perl.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
 
+export PERLBREW_ROOT=${HOME}/.perl5/perlbrew
+export MY_PERL_LOCALLIB_ROOT=${HOME}/.perl5/locallib
+
 function use_rakudo() {
     use RAKUDO_HOME /opt/personal/pkg/rakudo/install
 }
 
 function use_default_locallib() {
     export PRE_LOCALLIB_PATH=$PATH
-    export PATH="${HOME}/perl5/locallib/default/bin:$PATH"
+    export PATH="${MY_PERL_LOCALLIB_ROOT}/default/bin:$PATH"
     unset PERL5LIB
-    eval $(perl -Mlocal::lib=${HOME}/perl5/locallib/default)
+    eval $(perl -Mlocal::lib=${MY_PERL_LOCALLIB_ROOT}/default)
 }
 
 function use_perlbrew_no_default {
-    source ${HOME}/perl5/perlbrew/etc/bashrc
+    source ${PERLBREW_ROOT}/etc/bashrc
 }
 
 function use_perlbrew() {
@@ -24,7 +27,7 @@ function use_locallib() {
     # restore original path
     export PATH=$PRE_LOCALLIB_PATH
     unset PERL5LIB
-    eval $(perl -Mlocal::lib=$1)
+    eval $(perl -Mlocal::lib=${MY_PERL_LOCALLIB_ROOT}/$1)
 }
 
 # vim: sts=4 sw=4 ts=4 et ft=sh
