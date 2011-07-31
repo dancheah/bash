8ee45db80c8e8f304ce6dd686c6311ca15eab903
Daniel Cheah <me@dancheah.com>
2011-07-30 19:34:20 -0700

    use a default gemset

--- lib/source-ruby.sh
+++ lib/source-ruby.sh
@@ -2,6 +2,9 @@
 function use_rvm() {
     export CC=gcc-4.2
     [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+    # default gemset to use
+    rvm gemset use jekyll
 }
 
 # vim: sts=4 sw=4 ts=4 et ft=sh
