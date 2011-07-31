7fb729ab761b0486dbe6f64b9ce6c5b8c23276c1
Daniel Cheah <me@dancheah.com>
2011-07-30 17:27:43 -0700

    updated pythonbrew and virtualenv functions

--- lib/source-python.sh
+++ lib/source-python.sh
@@ -1,9 +1,20 @@
-function use_pythonbrew() {
+function use_pythonbrew {
+    use_pythonbrew_no_default
+    use_default_virtualenv
+}
+
+function use_pythonbrew_no_default {
+    export CC=gcc-4.2
     source ${HOME}/.pythonbrew/etc/bashrc
 }
 
+function use_default_virtualenv {
+    use_virtualenv
+    workon default
+}
+
 function use_virtualenv {
-    source ${HOME}/.pythonbrew/pythons/current/bin/virtualenvwrapper.sh
+    source `which virtualenvwrapper.sh`
 }
 
 # vim: sts=4 sw=4 ts=4 et ft=sh
