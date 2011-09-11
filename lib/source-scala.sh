40110c660d22e7a842ae36ea9dfef9adcf3768c2
Daniel C <dc@thinkpad.(none)>
2011-09-10 23:08:32 -0700

    added scala

--- lib/source-scala.sh
+++ lib/source-scala.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-function use_scala() {
+function use_typesafe() {
     use SCALA_HOME /opt/personal/pkg/typesafe-stack
     export AKKA_HOME=/opt/personal/pkg/typesafe-stack
+}
+
+function use_scala() {
+    use SCALA_HOME /opt/personal/pkg/scala
 }
 
 # vim: sts=4 sw=4 ts=4 et ft=sh
