7a6614ab8f8ec22d108dc6dc05505da6cd76c441
Daniel Cheah <me@dancheah.com>
2011-09-18 11:24:28 -0700

    added jrebel function

--- lib/source-scala.sh
+++ lib/source-scala.sh
@@ -9,4 +9,14 @@ function use_scala() {
     use SCALA_HOME /opt/personal/pkg/scala
 }
 
+function use_scalate() {
+    use SCALATE_HOME /opt/personal/pkg/scalate-1.5.2
+}
+
+function use_jrebel() {
+    export JREBELHOME=/opt/personal/pkg/ZeroTurnaround/JRebel
+    export SBT_OPTS="-noverify -javaagent:$JREBELHOME/jrebel.jar"
+    export MAVENOPTS="-noverify -javaagent:$JREBELHOME/jrebel.jar"
+}
+
 # vim: sts=4 sw=4 ts=4 et ft=sh
